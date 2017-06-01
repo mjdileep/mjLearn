f0500a13abd8ba63483ab31002451847aab3b331
mjdileep <dileep.jayamal@gmail.com>
2017-06-01 07:42:10 +0530

    commented test file

--- softmax_regressor_test.py
+++ softmax_regressor_test.py
@@ -7,11 +7,16 @@
 import  numpy  as np
 from lib import softmax_regressor as sr
 
+#X is the input set while Y is the target for the training set
 X=np.matrix([[4,2,3],[7,7,3],[2,12,4],[9,10,2],[7,16,9],[5,13,9],[2,25,15],[2,22,18]])
 Y=np.matrix([[1,0,0,0],[1,0,0,0],[0,1,0,0],[0,1,0,0],[0,0,1,0],[0,0,1,0],[0,0,0,1],[0,0,0,1]])
+
 var=sr.softMaxRegressor()
-var.train(X,Y,bandwidth=10,alpha=0.001,max_itr=1000)
-test=np.matrix([[2,12,4],[4,3,3]])
-rs=var.predict(test)
+var.train(X,Y,bandwidth=10,alpha=0.001,max_itr=1000)#here you have to keep the balance between bandwidth,alpha and max_itr
+
+test=np.matrix([[2,12,4],[4,3,3]])#test input
+
+rs=var.predict(test)#predict the results
+
 for i in rs:
     print(i)
